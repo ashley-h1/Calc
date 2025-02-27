00032ca97056b12a50c43773bca3b6f139821ca4
Ashley Huff <huffa0207@gmail.com>
2025-02-26 19:26:10 -0500

    updated runner to make sure input is a number

--- Runner.java
+++ Runner.java
@@ -7,12 +7,36 @@ public class Runner {
     public static void main(String[] args){
         //instantiate a Calc object
         Calc myCalculator = new Calc();
+        double n1 = 0;
+        double n2 = 0;
         //get user input for two numbers
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter the first number: ");
-        double n1 = scan.nextDouble();
-        System.out.println("Please enter the second number: ");
-        double n2 = scan.nextDouble();
+        int score = 0;
+        //input for num1
+        while (score == 0){
+            System.out.println("Please enter the first number: ");
+            String input1 = scan.nextLine();
+            if (checkInput(input1) == false){
+                System.out.println("Numbers only");
+            } 
+            else{
+                n1 = Double.parseDouble(input1);
+                score = 1;
+            }
+        //input for num2
+        }
+        while (score == 1){
+            System.out.println("Please enter the second number: ");
+            String input2 = scan.nextLine();
+            if (checkInput(input2) == false){
+                System.out.println("Numbers only");
+            }
+            else{
+                n2 = Double.parseDouble(input2);
+                score = 3;
+            }
+        }
+        scan.close();
 
         //pass the numbers to the Calc object
         myCalculator.setNum1(n1);
@@ -34,5 +58,17 @@ public class Runner {
         System.out.println("The difference is: " + myCalculator.subtract());
         System.out.println("The product is: " + myCalculator.multiply());
         System.out.println("The quotient is: " + myCalculator.divide());
+    }
+    public static boolean checkInput(String str){ 
+        boolean validInput = false;
+        try{
+            Double.parseDouble(str);
+            validInput = true;
+            return validInput;
+        } 
+        catch (NumberFormatException e){
+            validInput = false;
+            return validInput;
+        }
     }
 }
